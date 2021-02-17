6e792d1b1a2fcc9507a31c0ae7004d0b3c5972c1
codeclanstudent <sstewart1993@outlook.com>
2021-02-17 14:22:58 +0000

    css need sorted

--- js/app.js
+++ js/app.js
@@ -1,3 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('JavaScript loaded');
-  })
+  console.log('JavaScript loaded');
+
+
+  const handleFormSubmit = function(event){
+    event.preventDefault()
+
+    const newListItem = document.createElement('li');
+    newListItem.textContent = `Title: ${event.target.title.value} Author: ${event.target.time.value} Category:${event.target.genre.value}`
+    newListItem.classList.add('black', 'bold');
+
+    const list = document.querySelector('ul');
+    list.appendChild(newListItem);
+
+    event.target.title.value = ''
+    event.target.time.value = ''
+    event.target.genre.value = ''
+
+  }
+
+  const handleFormDelete = function(){
+    // let list = [];
+    let movieList = document.querySelector('ul');
+    movieList.innerHTML= ""
+    
+    
+  }
+
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleFormSubmit)
+
+  const list = document.querySelector('#delete');
+  list.addEventListener('click', handleFormDelete)
+
+
+
+})
+
